66574b6b90034d28c3b50ebade5c44c2f562da60
Brady <92948316+bradychin@users.noreply.github.com>
2026-05-19 00:11:38 -0400

    Initial Commit

---
+++ workouts/workoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  workoutsApp.swift
+//  workouts
+//
+//  Created by Brady Chin on 2026.05.19.
+//
+
+import SwiftUI
+
+@main
+struct workoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
